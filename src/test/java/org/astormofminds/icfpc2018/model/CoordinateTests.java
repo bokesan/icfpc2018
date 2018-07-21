5a702e98cc25e4daf0e2cfe66fb0b611c48b9364
Christoph Breitkopf <chbreitkopf@gmail.com>
2018-07-21 03:47:53 +0200

    Add tests

--- src/test/java/org/astormofminds/icfpc2018/model/CoordinateTests.java
+++ src/test/java/org/astormofminds/icfpc2018/model/CoordinateTests.java
@@ -2,6 +2,7 @@ package org.astormofminds.icfpc2018.model;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -25,4 +26,17 @@ public class CoordinateTests {
         assertFalse(c1.isAdjacentTo(c1));
     }
 
+    @Test
+    public void testPlus() {
+        Coordinate c = Coordinate.of(10, 20, 30);
+        Difference d = Difference.of(5, -5, 7);
+        assertEquals(Coordinate.of(15, 15, 37), c.plus(d));
+    }
+
+    @Test
+    public void testPlusLarge() {
+        Coordinate c = Coordinate.of(0, 249, 22);
+        Difference d = Difference.of(249, -249, 7);
+        assertEquals(Coordinate.of(249, 0, 29), c.plus(d));
+    }
 }
