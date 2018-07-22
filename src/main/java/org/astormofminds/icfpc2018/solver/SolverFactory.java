d8e5898c8cf03f5d8fd6d7647ef17d059508130e
Jan Dreske <jandreske@web.de>
2018-07-22 17:08:28 +0200

    swarm bot number fix

--- src/main/java/org/astormofminds/icfpc2018/solver/SolverFactory.java
+++ src/main/java/org/astormofminds/icfpc2018/solver/SolverFactory.java
@@ -6,10 +6,6 @@ public class SolverFactory {
 
     public static Solver byName(String name) {
         switch (name) {
-            case "stupid":  return new StupidSolver();
-            case "stupidbounding":  return new StupidBoundingSolver();
-            case "optimizing": return new OptimizedSbSolver();
-            case "lowharm": return new LowHarmonicSolver();
             case "lhmulti": return new LHMultiFiller();
             case "moveto": return new LayerSolver();
             case "twin": return new TwinSolver();
